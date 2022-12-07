8dccd569c06d29f717388b28b163bac1ff1afa48
Tommy Bidne <tbidne@protonmail.com>
2022-12-07 20:04:27 +1300

    Swap flake-utils for flake-parts

--- flake.nix
+++ flake.nix
@@ -6,20 +6,20 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # haskell
     algebra-simple = {
       url = "github:tbidne/algebra-simple";
       inputs.flake-compat.follows = "flake-compat";
-      inputs.flake-utils.follows = "flake-utils";
+      inputs.flake-parts.follows = "flake-parts";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     bounds = {
       url = "github:tbidne/bounds";
       inputs.flake-compat.follows = "flake-compat";
-      inputs.flake-utils.follows = "flake-utils";
+      inputs.flake-parts.follows = "flake-parts";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     byte-types = {
@@ -27,7 +27,7 @@
       inputs.algebra-simple.follows = "algebra-simple";
       inputs.bounds.follows = "bounds";
       inputs.flake-compat.follows = "flake-compat";
-      inputs.flake-utils.follows = "flake-utils";
+      inputs.flake-parts.follows = "flake-parts";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     smart-math = {
@@ -35,13 +35,13 @@
       inputs.algebra-simple.follows = "algebra-simple";
       inputs.bounds.follows = "bounds";
       inputs.flake-compat.follows = "flake-compat";
-      inputs.flake-utils.follows = "flake-utils";
+      inputs.flake-parts.follows = "flake-parts";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     time-conv = {
       url = "github:tbidne/time-conv";
       inputs.flake-compat.follows = "flake-compat";
-      inputs.flake-utils.follows = "flake-utils";
+      inputs.flake-parts.follows = "flake-parts";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -50,53 +50,56 @@
     , bounds
     , byte-types
     , flake-compat
-    , flake-utils
+    , flake-parts
     , nixpkgs
     , self
     , smart-math
     , time-conv
     }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      buildTools = c: with c; [
-        cabal-install
-        pkgs.gnumake
-        pkgs.zlib
-      ];
-      devTools = c: with c; [
-        ghcid
-        haskell-language-server
-      ];
-      ghc-version = "ghc924";
-      compiler = pkgs.haskell.packages."${ghc-version}";
-      mkPkg = returnShellEnv: withDevTools:
-        compiler.developPackage {
-          inherit returnShellEnv;
-          name = "pythia";
-          root = ./.;
-          modifier = drv:
-            pkgs.haskell.lib.addBuildTools drv
-              (buildTools compiler ++
-                (if withDevTools then devTools compiler else [ ]));
-          overrides = final: prev: with compiler; {
-            algebra-simple =
-              final.callCabal2nix "algebra-simple" algebra-simple { };
-            bounds =
-              final.callCabal2nix "bounds" bounds { };
-            byte-types =
-              final.callCabal2nix "byte-types" byte-types { };
-            smart-math =
-              final.callCabal2nix "smart-math" smart-math { };
-            time-conv =
-              final.callCabal2nix "time-conv" time-conv { };
-          };
+    flake-parts.lib.mkFlake { inherit self; } {
+      perSystem = { pkgs, ... }:
+        let
+          buildTools = c: with c; [
+            cabal-install
+            pkgs.gnumake
+            pkgs.zlib
+          ];
+          devTools = c: with c; [
+            ghcid
+            haskell-language-server
+          ];
+          ghc-version = "ghc924";
+          compiler = pkgs.haskell.packages."${ghc-version}";
+          mkPkg = returnShellEnv: withDevTools:
+            compiler.developPackage {
+              inherit returnShellEnv;
+              name = "pythia";
+              root = ./.;
+              modifier = drv:
+                pkgs.haskell.lib.addBuildTools drv
+                  (buildTools compiler ++
+                    (if withDevTools then devTools compiler else [ ]));
+              overrides = final: prev: with compiler; {
+                algebra-simple =
+                  final.callCabal2nix "algebra-simple" algebra-simple { };
+                bounds =
+                  final.callCabal2nix "bounds" bounds { };
+                byte-types =
+                  final.callCabal2nix "byte-types" byte-types { };
+                smart-math =
+                  final.callCabal2nix "smart-math" smart-math { };
+                time-conv =
+                  final.callCabal2nix "time-conv" time-conv { };
+              };
+            };
+        in
+        {
+          packages.default = mkPkg false false;
+          devShells.default = mkPkg true true;
+          devShells.ci = mkPkg true false;
         };
-    in
-    {
-      packages.default = mkPkg false false;
-
-      devShells.default = mkPkg true true;
-      devShells.ci = mkPkg true false;
-    });
+      systems = [
+        "x86_64-linux"
+      ];
+    };
 }
