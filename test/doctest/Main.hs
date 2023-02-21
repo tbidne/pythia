24e8261aaebc048ea49d76e1eb6162620e5058f1
Tommy Bidne <tbidne@protonmail.com>
2023-02-22 10:45:17 +1300

    Swap doctest-parallel for doctest

--- test/doctest/Main.hs
+++ test/doctest/Main.hs
@@ -9,5 +9,62 @@ main =
   guardOrElse'
     "RUN_DOCTEST"
     ExpectEnvSet
-    (DocTest.mainFromCabal "pythia" [])
+    (DocTest.doctest args)
     (putStrLn "*** Doctests Disabled. Enable with RUN_DOCTEST=1. ***")
+  where
+    args = files <> exts
+
+files :: [String]
+files =
+  [ "-isrc",
+    "src/Pythia.hs",
+    "src/Pythia/Prelude.hs",
+    "src/Pythia/Services/Battery/Acpi.hs",
+    "src/Pythia/Services/Battery/Types.hs",
+    "src/Pythia/Services/Battery/UPower.hs",
+    "src/Pythia/Services/GlobalIp/Types.hs",
+    "src/Pythia/Services/NetInterface/Ip.hs",
+    "src/Pythia/Services/NetInterface/NmCli.hs",
+    "src/Pythia/Services/NetInterface/Types.hs",
+    "src/Pythia/Services/Time.hs",
+    "src/Pythia/Services/Types/Network.hs",
+    "src/Pythia/Utils.hs"
+  ]
+
+exts :: [String]
+exts =
+  [ "-XNoImplicitPrelude",
+    "-XNoStarIsType",
+    "-XAllowAmbiguousTypes",
+    "-XApplicativeDo",
+    "-XConstraintKinds",
+    "-XDataKinds",
+    "-XDeriveAnyClass",
+    "-XDeriveFunctor",
+    "-XDeriveGeneric",
+    "-XDerivingVia",
+    "-XDuplicateRecordFields",
+    "-XFlexibleContexts",
+    "-XFlexibleInstances",
+    "-XFunctionalDependencies",
+    "-XGADTs",
+    "-XGeneralizedNewtypeDeriving",
+    "-XImportQualifiedPost",
+    "-XInstanceSigs",
+    "-XLambdaCase",
+    "-XMultiParamTypeClasses",
+    "-XMultiWayIf",
+    "-XNamedFieldPuns",
+    "-XNumericUnderscores",
+    "-XOverloadedLabels",
+    "-XOverloadedStrings",
+    "-XPatternSynonyms",
+    "-XRankNTypes",
+    "-XScopedTypeVariables",
+    "-XStandaloneDeriving",
+    "-XStandaloneKindSignatures",
+    "-XTupleSections",
+    "-XTypeApplications",
+    "-XTypeFamilyDependencies",
+    "-XTypeOperators"
+  ]
