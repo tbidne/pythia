03e733d45932ba74f86ce4bdbbd89d176df65090
Tommy Bidne <tbidne@protonmail.com>
2024-05-08 10:59:54 +1200

    Allow with nix aarch64

--- flake.nix
+++ flake.nix
@@ -111,7 +111,9 @@
           };
         };
       systems = [
-        "x86_64-darwin"
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
         "x86_64-linux"
       ];
     };
